41169564648b70a37d6f34de0b8cacd50542524f
kastillawhite <80781439+kastillawhite@users.noreply.github.com>
2026-08-04 20:14:02 -0700

    homepage changes

---
+++ js/navigation.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+if (hamburger && navLinks) {
+
+    hamburger.addEventListener("click", () => {
+
+        hamburger.classList.toggle("active");
+        navLinks.classList.toggle("open");
+
+    });
+
+}
